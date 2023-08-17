f1f6437df9ccbc97021ac2db3f9849a2385e71d1
nicovui <github.com>
2023-08-17 14:56:13 +0200

    remobe build folder

--- common/build/errors/not-found-error.d.ts
+++
@@ -1,9 +0,0 @@
-import { CustomError } from './custom-error';
-export declare class NotFoundError extends CustomError {
-    statusCode: number;
-    constructor();
-    serializeErrors(): {
-        message: string;
-        field?: string | undefined;
-    }[];
-}
